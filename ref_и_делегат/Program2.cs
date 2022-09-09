fd3ee110bda96f4488a20052d82899e2da3d9159
karabelum <saturnday@yandex.ru>
2022-09-09 21:03:56 +0500

    test

--- ref_и_делегат/Program2.cs
+++ ref_и_делегат/Program2.cs
@@ -6,17 +6,33 @@ using System.Threading.Tasks;
 
 namespace ref_и_делегат {
     internal class Program2 {
-        
+        class MyClass {
+            public string  { get; set; }
+        public string num1 { get; set; }
+        public string num2 { get; set; }
+        public string num3 { get; set; }
+        public MyClass(int num1, int num2, int num3) {
+
+        }
     }
 }
 Console.WriteLine("ввести несколько слов через пробел");
 //List<string> listslova = Console.ReadLine().Split(' ').ToList();
-List<string> listslova = new() { "Сова", "Ворон", "Сова", "Ястреб" };
+List<string> listslova = new() { "Сова", "Ворон", "Сова", "Ястреб", "Ястреб" };
 var c = listslova.GroupBy(x => x);
 //var z = c.OrderByDescending();
-
-    
-
-foreach (var item in c) {
-    Console.WriteLine(item.First() + " " + item.Count());
+foreach (var item in c.OrderByDescending(z=>z.Count()) {
+    Console.WriteLine(item.First() + "  " + item.Count());
 }
+List<MyClass> list = new() {
+    new MyClass(2,4,6)
+    new MyClass(4,5,7)
+    new MyClass(2,4,6)
+    new MyClass(12,22,6)
+    new MyClass(2,33,6)
+}
+foreach (var item in OrderByDescending(m=>m.num1)){
+    Console.WriteLine(item.ToString());
+}
+
+Console.WriteLine();
