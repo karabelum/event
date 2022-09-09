91459a6e6f891a98a3b16af48d8223006510ae1b
З - 7 < - 7@DESKTOP-LB7VAEG>
2022-09-09 19:37:34 +0500

    Добавьте файлы проекта.

---
+++ ref_и_делегат/Class1.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ref_и_делегат {
+
+    class Acc {
+        public delegate void AccHandler(string sender);
+
+        public event AccHandler? Notify;
+
+        internal class Class1 {
+            public Acc(int sum) {
+                sum = Sum;
+            }
+            public int Sum { get; set; }
+            public void Put(int sum) {
+                Sum += sum;
+                Notify?.Invoke(&"На счет поступило: {sum}");
+                //Console.WriteLine(&"На счет поступило: {sum}");
+            }
+            public void Take(int sum) {
+                if (Sum <= sum) {
+                    Sum -= sum;
+                    Notify?.Invoke(&"Со счета снято: {sum}");
+                }
+                else {
+                    Notify?.Invoke(&"Недостаточно средств");
+                }
+            }
+        }
+    }
---
+++ ref_и_делегат/Program.cs
@@ -0,0 +1,17 @@
+﻿using ref_и_делегат;
+void PrintMes() {
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Writeline(mes);
+}
+
+
+Acc acc1 = new Acc(100);
+acc1.Notify += PrintMes;
+
+
+acc1.Put(20);
+
+
+acc1.Take(700);
+
+acc1.Take(1800);
